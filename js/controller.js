b4ea1b75af71faa695cdb6254ea467ba8e565625
Maxime Rey <maxime.rey1@etu.univ-grenoble-alpes.fr>
2024-03-22 16:00:58 +0100

    s

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+// ### Gestion du bouton de recherche ###
+view.rechercheBtn.addEventListener("click" , () => {
+    view.rechercheDiv.classList.add("hide");
+    view.resultatDiv.classList.remove("hide");
+})
---
+++ js/view.js
@@ -0,0 +1,13 @@
+/**
+ * Objet constant représentant la vue.
+ */
+const view= {
+    rechercheBtn : document.getElementById("recherche_btn"),
+    rechercheDiv : document.getElementById("recherche_div"),
+
+    resultatDiv : document.getElementById("resultat_div"),
+
+    accueilBtn : document.getElementById("accueil_btn"),
+    
+    
+};
