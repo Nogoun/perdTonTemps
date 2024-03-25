c2c1414b15bd7f7dcf57e4924a2777a71f533970
Maxime Rey <maxime.rey1@etu.univ-grenoble-alpes.fr>
2024-03-25 17:25:33 +0100

    j

--- js/controller.js
+++ js/controller.js
@@ -39,32 +39,18 @@ view.rechercheBtn.addEventListener("click", async function () {
 
         for (let clipInfo of clipsInfo){
             //Création de la ligne du tableau
-            let ligne = document.createElement("tr");
+            let ligne = creationLigne(clipInfo.url , clipInfo.title , clipInfo.duration);
             
-            //Création de la première colonne avec un lien
-            let celluleLien = document.createElement("td");
-            let lien = document.createElement("a");
-            lien.href = clipInfo.url;
-            lien.textContent = clipInfo.title;
-            lien.target = "_blank"; // Ouvre le lien dans un nouvel onglet
-            celluleLien.appendChild(lien);
-
-            //Création de la deuxième colonne avec la durée de la vidéo
-            let celluleDuree = document.createElement("td");
-            celluleDuree.textContent = clipInfo.duration + " secondes";
-
-            //Ajout des deux cellules à la ligne
-            ligne.appendChild(celluleLien);
-            ligne.appendChild(celluleDuree);
-            ligne.classList.add("ligne_tableau");
-
-
             //Ajout de la ligne au tableau.
             tcorps.appendChild(ligne);
             console.log("fin de la boucle chaine");
 
             //Mise à jour de la  valeur du temps 
             tempsPerdu += clipInfo.duration;
+        }
+
+        if (localStorage.getItem(memoryString) == null){
+            localStorage.setItem(memoryString , JSON.stringify(clipsInfo));
         }
         
     }
@@ -78,7 +64,7 @@ view.rechercheBtn.addEventListener("click", async function () {
     //Création de la première colonne avec un texte
     let celluleTexte = document.createElement("td");
     let texte = document.createElement("p");
-    texte.textContent = "Le temps que vous avez perdu est de :";
+    texte.textContent = "Le temps que vous pouvez perdre est de :";
     celluleTexte.appendChild(texte);
 
     //Création de la deuxième colonne avec la durée totale des clips
@@ -100,7 +86,42 @@ view.rechercheBtn.addEventListener("click", async function () {
     view.resultatDiv.classList.remove("hide");
 });
 
+/**
+ * Prends en paramètre le lien d'un clip , le titre du clip et le temps du clip.
+ * Renvoie une ligne qui est composé d'un lien qui a pour texte le titre du clip et dans une autre colonne la durée de la vidéo
+ */
+function creationLigne(href , textContent1 , textContent2){
+    //Création de la ligne du tableau
+    let ligne = document.createElement("tr");
+            
+    //Création de la première colonne avec un lien
+    let celluleLien = document.createElement("td");
+    let lien = document.createElement("a");
+    lien.href = href;
+    lien.textContent = textContent1;
+    lien.target = "_blank"; // Ouvre le lien dans un nouvel onglet
+    celluleLien.appendChild(lien);
 
+    //Création de la deuxième colonne avec la durée de la vidéo
+    let celluleDuree = document.createElement("td");
+    celluleDuree.textContent = textContent2 + " secondes";
+
+    //Création de la troisième colonne pour le bouton des favoris
+    let celluleFavoris = document.createElement("td");
+    let favorisBtn = document.createElement("button");
+    favorisBtn.classList.add("favoris_btn");
+    favorisBtn.textContent = "+";
+    favorisBtn.value = href;
+    favorisBtn.addEventListener("click" , favorisClickListenner);
+    celluleFavoris.appendChild(favorisBtn);
+
+    //Ajout des deux cellules à la ligne
+    ligne.appendChild(celluleLien);
+    ligne.appendChild(celluleDuree);
+    ligne.appendChild(celluleFavoris);
+    ligne.classList.add("ligne_tableau");
+    return ligne;
+}
 
 view.accueilBtn.addEventListener("click" , function() {
     view.rechercheDiv.classList.remove("hide");
@@ -108,7 +129,52 @@ view.accueilBtn.addEventListener("click" , function() {
     view.msgAttente.classList.add("hide");
 });
 
+/**
+     * Situtaion différente en fonction du textContent du bouton
+     * Si c'est un "+" cela signifie que le lien n'a pas été ajouté aux favoris
+     * Si c'est un "x" cela signifie que le lien est présent dans les favoris
+*/
+let favorisClickListenner = function (event) {
+    const value = event.target.value;
+    
+    
+    if (event.target.textContent == "+"){
+        let listeFavoris = localStorage.getItem("favoris");
+        
+        
+        if (listeFavoris == null){
+            localStorage.setItem("favoris" , [value]);
+        }else {
+            let present = false;
+            arrayFavoris = listeFavoris.split(",");
+            for (favori of arrayFavoris){
+                
+                if (favori == value){
+                    present = true;
+                }
+            }
+            if (!present){
+                arrayFavoris.push(value);
+                localStorage.setItem("favoris" , arrayFavoris);
+            }
 
-view.favorisBtn.addEventListener("click" , function () {
+        }
+        event.target.textContent = "x";
+    }else {
+        let listeFavoris = localStorage.getItem("favoris");
+        arrayFavoris = listeFavoris.split(",");
+        let compteur = 0;
+        for (favoris of arrayFavoris){
+            if (value == favoris){
+                console.log(arrayFavoris);
+                arrayFavoris.splice(compteur , 1);
+            }
+            compteur += 1;
+        }
+        localStorage.setItem("favoris" , arrayFavoris);
 
-})
+
+        event.target.textContent = "+";
+    }
+    
+}
